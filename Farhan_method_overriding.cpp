b5c140ffc0c9858a4a3639628a850e7aae92b417
farhanAshraf20 <98956649+farhanAshraf20@users.noreply.github.com>
2022-02-23 16:36:52 +0530

    Add files via upload

---
+++ Farhan_method_overriding.cpp
@@ -0,0 +1,31 @@
+/*
+*write program cpp program using method overriding
+*Date : 18-feb-2022
+*Farhan AShraf
+*/
+
+#include <iostream>
+using namespace std;
+class A
+{
+   public:
+   void show(int x,int y)
+   {
+       cout<<"sum="<<x+y<<endl;
+     
+   }
+};
+class B: public A
+{
+    public:
+    void show(int x,int y)
+    {
+        cout<<"Multi="<<x*y<<endl;
+    }
+};
+int main()
+{
+    A b;
+    b.show(15,2);
+    
+    }
